a716c66b724bb550a8b937320e2173e1ddc10da2
Xellophane <rinotfox@gmail.com>
2015-04-06 15:10:38 -0600

    Added Network

---
+++ SonicPiledriver/Network.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SonicPiledriver;
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author Xellophane
+ */
+public class Network {
+    
+    public Socket connect(String hostName, int port, Socket client) {
+        /* The connect method simply connects to the host server
+        * and spits out a couple of error messages. should be able
+        * to replace hard codded variables with options eventually.
+        */
+        try {
+        System.out.println("Connecting to  " + hostName + " on port " + port);
+        client = new Socket(hostName, port);
+        System.out.println("Just connected to " + client.getRemoteSocketAddress());
+        return client;
+        } catch (UnknownHostException e) {
+            System.out.print("Unknown host " + e);
+            System.exit(-1);
+        } catch (IOException e) {
+            System.out.println("IOException : " + e);
+            System.exit(-2);
+        }
+        
+        return null;
+    }
+}
